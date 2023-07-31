1aab23a5dfc7d08065060c9e8f561f8ff173042d
Júlio Becker <juliobeckinha@gmail.com>
2023-07-31 18:50:16 -0300

    Fix Login

---
+++ ADMINuser.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (username, password, role)
+VALUES ('admin', '$2a$10$EYTmILwlfKymf0nmvBXy0Oxf9N36EAS35j5nnFW/TBiqhvK4UlduW', 0);
--- ms-products/src/main/java/br/com/compassuol/sp/challenge/msproducts/config/RabbitConfig.java
+++ ms-products/src/main/java/br/com/compassuol/sp/challenge/msproducts/config/RabbitConfig.java
@@ -1,13 +1,9 @@
 package br.com.compassuol.sp.challenge.msproducts.config;
 
-import br.com.compassuol.sp.challenge.msproducts.rabbitmq.MyResponseHandler;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
